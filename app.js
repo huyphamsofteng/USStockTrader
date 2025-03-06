6ba0c1e20d6c263d23b731b20827029a08f48ee1
harry.pham <phamd18@mcmaster.ca>
2025-03-06 10:01:36 -0500

    Add view template as Mustache and improve front-end interface

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress(__dirname + '/views' + '/partials', '.mustache'));
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.get("/resources/bg_cover", async (req, res) => {
+    const img_path = __dirname + "/resources/bg-cover.jpg";
+    res.sendFile(img_path);
+});
+
+app.get("/resources/favicon", async (req, res) => {
+    const icon_path = __dirname + "/resources/favicon.ico";
+    res.sendFile(icon_path);
+});
 
 
 
