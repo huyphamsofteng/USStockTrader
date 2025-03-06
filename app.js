7319d301228f26e43f13f64446360399246b2f23
harry.pham <phamd18@mcmaster.ca>
2025-03-06 09:26:07 -0500

    Framed the main page and the portfolio page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+
+
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
